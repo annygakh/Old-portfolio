f8308bf9e9d951e6b6d9b9b5a69850fd8a8993fd
Anny Gakhokidze <annygakhokidze@gmail.com>
2015-02-26 14:54:52 -0800

    changed news reader to flipboard

--- app.js
+++ app.js
@@ -1,23 +1,54 @@
-var main = function (){
-	$('.article').click(function(){
-		$('.article').removeClass('current');
-		$('.description').hide();
-		$(this).addClass('current');
-		$(this).children('.description').show(); 
-	});
+var main = function() {
+  $('.dropdown-toggle').click(function() {
+    $('.dropdown-menu').toggle();
+  });
 
-	$(document).keypress(function(event){
-		if (event.which === 111)
-			$('.current').children('.description').toggle();
-		else if(event.which === 110) {
-			var currentArticle = $('.current');
-			var nextArticle = currentArticle.next();
+  
+  $('.arrow-next').click(function() {
+    var currentSlide = $('.active-slide');
+    var nextSlide = currentSlide.next();
 
-			currentArticle.removeClass('current');
-			nextArticle.addClass('current');
-		}
-	});
+    var currentDot = $('.active-dot');
+    var nextDot = currentDot.next();
 
-};
+    if(nextSlide.length === 0) {
+      nextSlide = $('.slide').first();
+      nextDot = $('.dot').first();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    nextSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    nextDot.addClass('active-dot');
+  });
+
+
+  $('.arrow-prev').click(function() {
+    var currentSlide = $('.active-slide');
+    var prevSlide = currentSlide.prev();
+
+    var currentDot = $('.active-dot');
+    var prevDot = currentDot.prev();
+
+    if(prevSlide.length === 0) {
+      prevSlide = $('.slide').last();
+      prevDot = $('.dot').last();
+    }
+    
+    currentSlide.fadeOut(600).removeClass('active-slide');
+    prevSlide.fadeIn(600).addClass('active-slide');
+
+    currentDot.removeClass('active-dot');
+    prevDot.addClass('active-dot');
+  });
+
+
+  $(document).keypress(function(event){
+  		if (event.which === 32)
+  			$('.dropdown-menu').hide();
+  });
+
+}
 
 $(document).ready(main);
